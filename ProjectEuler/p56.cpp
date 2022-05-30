e9363d9cba080e914d6d24feae0971c7ce53f967
W10 <W10@gmail.com>
2022-05-31 02:05:27 +0800

    1

--- ProjectEuler/p56.cpp
+++ ProjectEuler/p56.cpp
@@ -1,5 +1,6 @@
 /*
-
+A googol (10<sup>100</sup>) is a massive number: one followed by one-hundred zeros; 100<sup>100</sup> is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only 1.
+Considering natural numbers of the form, <i>a<sup>b</sup></i>, where <i>a, b</i> &lt; 100, what is the maximum digital sum?
 */
 
 #include <algorithm>
@@ -19,32 +20,87 @@
 
 using namespace std;
 
-int power(int a, int b, int m)
+void mul(vector<int> &a, vector<int> &b)
 {
-	int ret = 1;
+	int m = a.size();
+	int n = b.size();
+	vector<int> tmp(m + n);
+
+	for (int i = 0; i < m; ++i)
+		for (int j = 0; j < n; ++j)
+			tmp[i + j] += a[i] * b[j];
+
+	a.resize(m + n);
+	int c = 0;
+
+	for (int k = 0; k < tmp.size(); ++k)
+	{
+		a[k] = tmp[k] + c;
+		c = a[k] / 10;
+		a[k] %= 10;
+	}
+
+	while (c)
+	{
+		a.push_back(c);
+		c = a.back() / 10;
+		a.back() %= 10;
+	}
+
+	while (a.size() && a.back() == 0)
+		a.pop_back();
+}
+
+void power(vector<int> &a, int b)
+{
+	vector<int> ret = {1};
 
 	while (b)
 	{
 		if (b & 1)
-			ret = (ret * a) % m;
+			mul(ret, a);
 
-		a = (a * a) % m;
+		mul(a, a);
 		b >>= 1;
+	}
+
+	a = ret;
+}
+
+vector<int> to_vec(int a)
+{
+	vector<int> ret;
+
+	while (a)
+	{
+		ret.push_back(a % 10);
+		a /= 10;
 	}
 
 	return ret;
 }
 
-int euler(int a, int b)
+int euler(int n)
 {
-	cout << power(2, 10, 10) << endl;
+	int ret = 0;
 
-	return 0;
+	for (int a = 2; a < n; ++a)
+		for (int b = 2; b < n; ++b)
+		{
+			auto va = to_vec(a);
+			power(va, b);
+
+			int ds = accumulate(begin(va), end(va), 0);
+
+			ret = max(ret, ds);
+		}
+
+	return ret;
 }
 
 int main()
 {
-	cout << euler(10, 10) << endl;
+	cout << euler(100) << endl;
 
 	return 0;
 }
